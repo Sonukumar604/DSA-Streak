d0cd18b114f8423ebae393404d4e8d7b6dbadd9a
Sonu Kumar <sonusaini48292@gmail.com>
2026-08-14 12:52:34 +0530

    Added the Divisible by 60 Maths problem solution from Maths Interview bit playlist

---
+++ #Streak5/DivisibleBy60.java
@@ -0,0 +1,21 @@
+
+public class DivisibleBy60 {
+    public int divisibleBy60(ArrayList<Integer> A) {
+        if(A.size() == 1 && A.get(0) ==  0){
+            return 1;
+        }
+        int sum = 0;
+        boolean hasZero = false;
+        int evenCount = 0;
+        
+        for(int digit : A){
+            sum += digit;
+            if(digit == 0) hasZero = true;
+            if(digit % 2 == 0) evenCount++;
+        }
+        if(hasZero && (sum % 3 == 0) && evenCount >= 2){
+            return 1;
+        }
+        return 0;
+    }
+}
