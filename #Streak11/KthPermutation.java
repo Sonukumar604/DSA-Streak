d9b2f980de20f255490089ed2cb0bc2c5f685cc4
Sonu Kumar <sonusaini48292@gmail.com>
2026-08-20 15:17:27 +0530

    Updated the kthPermutation problem-solution by optimizing its time and space ccomplexity

--- #Streak11/KthPermutation.java
+++ #Streak11/KthPermutation.java
@@ -1,32 +1,27 @@
 public class KthPermutation {
-    public ArrayList<Integer> findPerm(int A, long B) {
-        
+    public ArrayList<Integer> findPerm(int A, Long B) {
         ArrayList<Integer> result = new ArrayList<>();
         ArrayList<Integer> numbers = new ArrayList<>();
-
-        // Fill numbers list with 1 to A
-        for (int i = 1; i <= A; i++) {
-            numbers.add(i);
-        }
-
-        // Convert B to 0-based index
-        long k = B - 1;
-
-        // If A > 20, the first (A - 20) elements will just be 1, 2, 3... 
-        // because 20! > 10^18 (max value of B)
+        
+       int cur = 1;
         while (A > 20) {
-            result.add(numbers.remove(0));
+            result.add(cur);
+            cur++;
             A--;
         }
-
-        // Precompute factorials for up to 20 numbers
+        
+        long k = B - 1;
+        
+        for (int i = cur; i < cur + A; i++) {
+            numbers.add(i);
+        }
+        
         long[] fact = new long[A + 1];
         fact[0] = 1;
         for (int i = 1; i <= A; i++) {
             fact[i] = fact[i - 1] * i;
         }
-
-        // Apply the index division logic for the remaining elements
+        
         for (int i = A; i >= 1; i--) {
             long blockSize = fact[i - 1];
             int index = (int) (k / blockSize);
